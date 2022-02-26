058216933645f963d4e062dea112e12baaa04244
Ibrahim Abu Seada <64257417+abuseada@users.noreply.github.com>
2022-02-26 09:51:35 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const navMenu = document.getElementById("nav-menu"),
+    toggleMenu = document.getElementById("toggle-menu"),
+    closeMenu = document.getElementById("close-menu");
+
+
+toggleMenu.addEventListener("click",() => [
+    navMenu.classList.toggle("active-menu")
+])
+closeMenu.addEventListener("click",() => [
+    navMenu.classList.remove("active-menu")
+]);
+
+/* ======================================================================== */
+
+var slideshows = document.querySelectorAll('[data-component="slideshow"]');
+slideshows.forEach(initSlideShow);
+
+function initSlideShow(slideshow) {
+
+	var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`);
+
+	var index = 0, time = 4000;
+	slides[index].classList.add('active');
+
+	setInterval( () => {
+		slides[index].classList.remove('active');
+		
+		index++;
+		if (index === slides.length) index = 0;
+
+		slides[index].classList.add('active');
+
+	}, time);
+}
